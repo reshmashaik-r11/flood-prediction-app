aafd4e069aa672b36ca981f2e3965eb8c6f612f9
Shaik Reshma <reshujani2007@gmail.com>
2026-07-12 00:48:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,3 +42,5 @@ def predict():
             return render_template('no_chance.html')
     except Exception as e:
         return f"Error: {str(e)}"
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
