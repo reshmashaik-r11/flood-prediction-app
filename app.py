64c954c5288cb0569e09cd4cf173ade21ab7d0c6
Shaik Reshma <reshujani2007@gmail.com>
2026-07-12 00:57:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,6 @@ def predict():
     except Exception as e:
         return f"Error: {str(e)}"
     if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
+    
